626c6d2dbd3c0d4b38ddf53ecbf01dc60e9a4959
IgnacioPM <ignacio040890@gmail.com>
2022-08-26 00:02:16 -0600

    adding UI for goals

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
+import path from 'path'
 import express from 'express'
 import 'dotenv/config'
 import colors from 'colors'
+import cors from 'cors'
 
 import { errorHandler } from './middleware/errorMiddleware.js';
 import { connectDB } from './config/db.js';
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 import postRoutes from './routes/goalRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -12,6 +26,7 @@ const app = express()
 const PORT = process.env.PORT || 8000
 connectDB()
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
